629d224d431392f87c0ea9242b4407d4fc00b878
tizianocoppoli <tiziano.coppoli@gmail.com>
2021-05-25 17:46:26 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Iv30rApuLBYy2SJEKSzjpjDmdUTZ7x4BG3k0pEJDyJNsw6Uia2pfQOBP6FEaO5zFifhzdZhKOWR42AiGTj39aIY00JywEkM8A',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
